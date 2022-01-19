dfe15d648a7c6727982c61f2498c1419dfec73be
devyosse <narutoshippuden884@gmail.com>
2022-01-19 13:42:40 +0100

    ewo 160

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
-
+$lorem = "Yo j'ai pas trouvé le lorem du coup voici ma chaine de caractère";
+$contenu = file_put_contents('fichier.txt', $lorem);
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
  * 4. Faites en sorte d'ajouter chaque chaîne de caractère de ce tableau au fichier créé dans la première partie ( point 2 )
@@ -15,20 +15,31 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
+$array = ["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix"];
 
+foreach ($array as $chaine){
+    echo file_put_contents("fichier.txt",  $chaine, FILE_APPEND);
+}
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
 
+echo nl2br(file_put_contents("fichier.txt", $array . "<br>"));
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+$dossier = "toto.txt";
+if (file_exists($dossier)){
+    echo "ce fichier existe bel et bien";
+}
+else{
+    echo "fichier inconnus";
+}
 
 /**
  * Super bonus.
